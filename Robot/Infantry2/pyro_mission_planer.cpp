931c326b400548b73c45647d1ba1cd803e6a6c1c
coaster <2038429915@qq.com>
2026-08-22 16:08:58 +0800

    init

--- Robot/Infantry2/pyro_mission_planer.cpp
+++ Robot/Infantry2/pyro_mission_planer.cpp
@@ -1,1 +1,18 @@
-#include ""
+#include "pyro_core_def.h"
+#include "pyro_core_config.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+extern "C"
+{
+    extern void pyro_init_thread(void *argument);
+    extern void start_debug_task(void *arg);
+
+    void start_mission_planer_task(void const *argument)
+    {
+        xTaskCreate(pyro_init_thread, "pyro_init_thread", 512, nullptr,
+                    configMAX_PRIORITIES - 1, nullptr);
+
+        vTaskDelete(nullptr);
+    }
+}
